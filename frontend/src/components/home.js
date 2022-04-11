86d9ac39dd9b31d8515d5cc4be3e4fbaee8d4cc1
Claire <clairezc27@gmail.com>
2022-04-11 13:23:20 -0400

    enhance home page UI

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,17 +1,42 @@
 import Button from '@mui/material/Button';
 import { Link } from "react-router-dom";
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@emotion/react';
 
 function Home () {
 
-    return (
-        <div>
-            <Link to="/signup">
-                <Button variant="contained">Signup</Button>
-            </Link>
+    const theme = createTheme({
+        status: {
+          danger: '#e53e3e',
+        },
+        palette: {
+          primary: {
+            main: '#0971f1',
+            darker: '#053e85',
+          },
+          white: {
+            main: '#FFFFFF',
+            contrastText: '##000000',
+          },
+          black: {
+            main: '#000000',
+            contrastText: '##FFFFFF',
+          },
+        },
+      });
 
-            <Link to="/login">
-                <Button variant="contained">Login</Button>
-            </Link>
+    return (
+        <div className="image">
+            <ThemeProvider theme={theme}>
+                <div className="text">
+                    <h1 className="title">MySousChef</h1>
+                    <p className="txt">Your personalized recipe search engine</p>
+                    <Link to="/login">
+                        <Button variant="contained" color="success">Get Started</Button>
+                    </Link>
+
+                </div>
+            </ThemeProvider>
             
         </div>
     );
